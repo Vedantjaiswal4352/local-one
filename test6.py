4fee5f885ec74d742dc7b25bf27dc54f1843da44
Vedant4352 <jaiswalvedant4352@gmail.com>
2023-02-11 11:58:11 +0530

    New 3 commit

--- test6.py
+++ test6.py
@@ -5,3 +5,6 @@ fig1, ax1 = plt.subplots()
 ax1.pie(sizes, labels=Partition, autopct='%1.1f%%', shadow=True, startangle=90)
 ax1.axis('equal')
 plt.show()
+x = [50,100,150]
+for i in x:
+    print(x**2)
